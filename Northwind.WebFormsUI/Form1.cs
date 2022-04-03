8c708feee8652982c00812c353becfb6e49d03a2
fkymon <mbtbrk11@outlook.com>
2022-04-03 15:16:23 +0300

    BTK 32.1

--- Northwind.WebFormsUI/Form1.cs
+++ Northwind.WebFormsUI/Form1.cs
@@ -1,4 +1,7 @@
-﻿using Northwind.Business.Concrete;
+﻿using Northwind.Business.Abstract;
+using Northwind.Business.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.DataAccess.Concrete.NHibernate;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,15 +16,17 @@ namespace Northwind.WebFormsUI
 {
     public partial class Form1 : Form
     {
+        IProductService _productService;
+
         public Form1()
         {
             InitializeComponent();
+            _productService = new ProductManager(new NhProductDal());
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ProductManager productManager = new ProductManager();
-           dgwProduct.DataSource = productManager.GetAll();
+            dgwProduct.DataSource = _productService.GetAll();
         }
     }
 }
